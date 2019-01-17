97eb6fce73030532bc80b98ffe4b6e49f7ec15f2
zhangzhengguo <zzg@hugehuge.cn>
2019-01-17 15:22:44 +0800

    添加currentThread()方法

---
+++ src/demo_190117_005/Demo_2.java
@@ -0,0 +1,19 @@
+package demo_190117_005;
+
+/**
+ * Java Class
+ * Created By Secondary
+ * On 2019/1/17 1:42 PM
+ * Description: currentThread()方法
+ */
+public class Demo_2 extends Thread {
+
+    /**
+     * main方法被名为main的线程调用
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+    }
+
+}
