a52fcec061f19b63372f66ff138aca37dafa32d7
zhangzhengguo <zzg@hugehuge.cn>
2019-04-28 21:28:13 +0800

    线程代码优化

--- src/demo190416013/SynchronizedDemo1.java
+++ src/demo190416013/SynchronizedDemo1.java
@@ -9,12 +9,14 @@ package demo190416013;
  */
 public class SynchronizedDemo1 {
 
+    private String str = "a";
+
     /**
      * 在使用suspend和resume方法时，如果使用不当，极易造成公共的同步对象的独占，使得其他线程无法访问公共同步对象
      */
     synchronized public void printString() {
         System.out.println("begin");
-        if (Thread.currentThread().getName().equals("a")) {
+        if (str.equals(Thread.currentThread().getName())) {
             System.out.println("a线程永远 suspend了");
             Thread.currentThread().suspend();
         }
@@ -24,23 +26,15 @@ public class SynchronizedDemo1 {
     public static void main(String[] args) {
         try {
             final SynchronizedDemo1 synchronizedDemo1 = new SynchronizedDemo1();
-            Thread thread1 = new Thread(){
-                @Override
-                public void run() {
-                    synchronizedDemo1.printString();
-                }
-            };
+            Thread thread1 = new Thread(() -> synchronizedDemo1.printString());
             thread1.setName("a");
             thread1.start();
             Thread.sleep(1000);
-            Thread thread2 = new Thread() {
-                @Override
-                public void run() {
-                    System.out.println("thread2启动了，但进入不了printString()方法！只打印1个begin");
-                    System.out.println("因为printString()方法被a线程锁定并且永远suspend暂停了");
-                    synchronizedDemo1.printString();
-                }
-            };
+            Thread thread2 = new Thread(() -> {
+                System.out.println("thread2启动了，但进入不了printString()方法！只打印1个begin");
+                System.out.println("因为printString()方法被a线程锁定并且永远suspend暂停了");
+                synchronizedDemo1.printString();
+            });
             thread2.start();
         } catch (InterruptedException e) {
             e.printStackTrace();
